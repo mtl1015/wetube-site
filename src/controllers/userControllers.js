582bca5d32cc9373edbab19d698a11ff17de2ffc
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-03-10 16:38:51 +0900

    2022-03-10
    seperate controller and router!

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,4 @@
+export const join =(req,res) => res.send("join");
+export const edit = (req,res) =>res.send("Edit User");
+export const deleteUser = (req,res) => res.send("Delete User");
+
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
+import {edit,deleteUser} from "../controllers/userControllers";
 
 const userRouter = express.Router();
 
-const handleEditUser = (req, res) => res.send("Edit User");
-const handleDelete = (req, res) => res.send("Delete User");
 
-userRouter.get("/edit", handleEditUser);
-userRouter.get("/delete", handleDelete);
+userRouter.get("/edit", edit);
+userRouter.get("/delete", deleteUser);
 
 export default userRouter;
