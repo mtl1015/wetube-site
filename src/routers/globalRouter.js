296100bcc2b7e42ed2830d7a942d317cabb6d04c
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-03-11 18:24:14 +0900

    install pug

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -4,8 +4,7 @@ import {trending,search} from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-const handleHome = (req,res) => res.send("Home");
-const handleJoin = (req,res) => res.send("Join");
+
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,7 +4,7 @@ import {edit,deleteUser, logout, see} from "../controllers/userControllers";
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get(":id", see);
+userRouter.get("/:id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", deleteUser);
 
