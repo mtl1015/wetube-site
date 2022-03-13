5a405a2e7d2be1928a34d0aeee553dd69b03521a
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-03-13 21:43:17 +0900

    learn block

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,9 @@
-export const trending = (req,res) => res.render("home");
+export const trending = (req,res) => res.render("home", {pageTitle: "Home"});
 export const see = (req,res) => {
     return res.render("watch");
 };
 
 export const upload = (req,res) => res.send("Upload Video");
-export const edit = (req,res) => res.send("Edit Video");
+export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search");
 export const deleteVideo = (req,res) => res.send("Delete Video");
