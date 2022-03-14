80e407fff6504d0f64780f3322d069ef2e1bd314
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-03-14 13:50:37 +0900

    learn mixin iteration conditional
    2022-03-14

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,31 @@
-export const trending = (req,res) => res.render("home", {pageTitle: "Home"});
+export const trending = (req,res) =>{
+    const videos = [
+        {
+            title: "First",
+            rating:5,
+            comment:2,
+            createdAt: "3 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second",
+            rating:7,
+            comment:1,
+            createdAt: "2 minutes ago",
+            views: 42,
+            id: 1,
+        },
+        {
+            title: "Third",
+            rating:9,
+            comment:6,
+            createdAt: "1 minutes ago",
+            views: 123,
+            id: 1,
+        }
+    ]
+    res.render("home", {pageTitle: "Home", videos});}
 export const see = (req,res) => {
     return res.render("watch");
 };
