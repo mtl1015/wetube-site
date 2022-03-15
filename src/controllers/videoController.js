8e0b6a573bfe1898c1d9f57bed7a2dde88a18398
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-03-15 12:48:35 +0900

    learning how to put database in back end
    2022-03-15

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,35 @@
+let videos = [
+    {
+        title: "First",
+        rating:5,
+        comment:2,
+        createdAt: "3 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second",
+        rating:7,
+        comment:1,
+        createdAt: "2 minutes ago",
+        views: 42,
+        id: 2,
+    },
+    {
+        title: "Third",
+        rating:9,
+        comment:6,
+        createdAt: "1 minutes ago",
+        views: 123,
+        id: 3,
+    }
+]
 export const trending = (req,res) =>{
-    const videos = [
-        {
-            title: "First",
-            rating:5,
-            comment:2,
-            createdAt: "3 minutes ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second",
-            rating:7,
-            comment:1,
-            createdAt: "2 minutes ago",
-            views: 42,
-            id: 1,
-        },
-        {
-            title: "Third",
-            rating:9,
-            comment:6,
-            createdAt: "1 minutes ago",
-            views: 123,
-            id: 1,
-        }
-    ]
     res.render("home", {pageTitle: "Home", videos});}
 export const see = (req,res) => {
-    return res.render("watch");
+    const {id} = req.params;
+    const video = videos[id - 1];
+    return res.render("watch",{pageTitle: `Watching ${video.title}`});
 };
 
 export const upload = (req,res) => res.send("Upload Video");
